5719630c7e358c5adfe63e6360ac8bffefa85768
MarlonMartins <marlonmartinsa7x@outlook.com>
2022-02-24 00:30:03 -0300

    Game stage

--- pacman/draw_pacman.py
+++ pacman/draw_pacman.py
@@ -1,28 +1,59 @@
 import pygame
 
-pygame.init()
-
-screen = pygame.display.set_mode((800, 600), 0)
-
 YELLOW = (255, 255, 0)
 BLACK = (0, 0, 0)
+WIDTH = 800
+HEIGHT = 600
+
+pygame.init()
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT), 0)
 
 
 class Pacman:
     def __init__(self):
+        self.column = 1
+        self.row = 1
         self.center_x = 400
         self.center_y = 300
-        self.size = 100
+        self.size = WIDTH // 30
+        self.speed_x = 1
+        self.speed_y = 1
+
         self.radius = int(self.size / 2)
 
-    def paint(self, screen):
+    def calculates_rules(self):
+        self.column = self.column + self.speed_x
+        self.row = self.row + self.speed_y
+        self.center_x = int(self.column * self.size + self.radius)
+        self.center_y = int(self.row * self.size + self.radius)
 
+        if self.center_x + self.radius > WIDTH:
+            self.speed_x = -1
+
+        if self.center_x - self.radius < 0:
+            self.speed_x = 1
+
+        if self.center_y + self.radius > HEIGHT:
+            self.speed_y = -1
+
+        if self.center_y - self.radius < 0:
+            self.speed_y = 1
+
+    def paint(self, screen):
+        if self.speed_x == -1:
+            direction = -1
+        else:
+            direction = 1
         pygame.draw.circle(
             screen, YELLOW, (self.center_x, self.center_y), self.radius
         )
         mouth_corner = (self.center_x, self.center_y)
-        upper_lip = (self.center_x + self.radius, self.center_y - self.radius)
-        lower_lip = (self.center_x + self.radius, self.center_y)
+        upper_lip = (
+            self.center_x + direction * self.radius,
+            self.center_y - self.radius,
+        )
+        lower_lip = (self.center_x + direction * self.radius, self.center_y)
         points = [mouth_corner, upper_lip, lower_lip]
         pygame.draw.polygon(screen, BLACK, points, 0)
 
@@ -38,9 +69,12 @@ if __name__ == "__main__":
     pacman = Pacman()
 
     while True:
+        pacman.calculates_rules()
+        screen.fill(BLACK)
 
         pacman.paint(screen)
         pygame.display.update()
+        pygame.time.delay(100)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
